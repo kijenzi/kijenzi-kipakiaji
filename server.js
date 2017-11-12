0201475c1029504a2518319be11be27709625143
Anthony Powell <apowell@arbor.net>
2017-11-12 16:51:01 -0500

    Create 'files' endpoint / scaffold endpoint arch
    - Scaffold endpoint architecture in the express server
    - Create and register '/api' endpoint
    - Create and register '/api/files' endpoint
    - Add body-parser npm package for POST processing

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
+// External libraries
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
+// Internal libraries
+const filesEndpoint = require('./server_modules/files');
+
+//-------------------------------------------------------------------
+// Tool initialization
+//-------------------------------------------------------------------
+
+// JSON parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Route capturing
+const router = express.Router();
+
+//-------------------------------------------------------------------
 // Define directories that a requester is allowed to access
+// For the front-end only!
+//-------------------------------------------------------------------
 app.use(express.static('build'));
 
-// Define captured routes
+//-------------------------------------------------------------------
+// Define captured api routes
+//-------------------------------------------------------------------
+
+// Register routes associated with the /api endpoint
+router.get('/', (req, res) => {
+	// Log the requesting IP addr
+	console.log('API GET Request received');
+	res.send('API GET Request Response');
+	});
+
+// Register routes associated with the /api/files endpoint
+filesEndpoint.RegisterRoutes(router);
+
+// Attach all routes to 'router' as sub-routes to '/api'
+app.use('/api', router);
+
+//-------------------------------------------------------------------
+// Serve static react bundle
+//-------------------------------------------------------------------
 app.get('/', (req, res) => {
 	// Send the main front-end bundle
 	const options = {
@@ -12,22 +50,14 @@ app.get('/', (req, res) => {
 	};
 
 	res.sendFile('index.html', options);
-})
-
-app.get('/api', (req, res) => {
-	// Log the requesting IP addr
-	console.log('API GET Request received');
-	res.send('API GET Request Response');
 });
 
-app.post('/api', (req, res) => {
-	// Log the requesting IP addr
-	console.log('API POST Request received');
-	res.send('API POST Request Response');
-});
-
+//-------------------------------------------------------------------
 // Start the server with the above configurations
+//-------------------------------------------------------------------
 app.listen(8080, () => {
 	// Print server IP, port, other info
 	console.log('Filehub is running on port 8080');
+	console.log('Access in your browser via: ');
+	console.log('    http://localhost/:8080');
 });
--- server_modules/files.js
+++ server_modules/files.js
@@ -5,35 +5,85 @@
  * eg. /api/{some_collection}/{particular_resource}/etc
  * 	collections and resources can be nested as it makes sense
  *
- * Here are some incomplete examples of the functionality we should handle.
- *
- * /api/files
- * Requests: GET, POST
- * 	GET: Return a listing of files
- * 		RESPONSE JSON: {
- * 			files: [
- * 				{
- * 					name: "Some File",
- * 					size: "123kb",
- * 					file_URI: "/api/files/{file:uid}/download"
- * 			  		thumb_URI: "/api/files/{file:uid}/thumb"
- * 				}
- * 			]
- * 		}
- * 	POST:
- * 		Figure out how to handle single file uploads. https://coligo.io/building-ajax-file-uploader-with-node/
- * 		looks very useful
- *
- *
- * /api/files/{file:uid}
- * Requests: GET
- * 	GET: Return a particular file's metadata
- * 		RESPONSE JSON: {
- * 			file: {
- * 				name: "Some File",
- * 				size: "123kb",
- * 				file_URI: "/api/files/{file:uid}/download',
- * 				thumb_URI: "/api/files/{file:uid}/thumb"
- * 			}
- * 		}
+ * Draft 'file' schema:
+ * {
+				name: (string) The name of the file,
+				size: (string) The filesize, with units preferably,
+				thumb_URI: (string) The location of the file's image,
+				file_URI: (string) Link to the un-modified file location
+				variables (optional): (array[object]) configurable parameters that will
+					 be converted into a Form input.
+					 Each object should contain the following properties:
+					 {
+						name: (string) The variable's name / input label,
+						description: (string) Description of the variable,
+						unit: (string) The unit of the variable's value,
+						min (optional): (int) If value is of type 'number', the min
+							possible value of that number,
+						max (optional): (int) If value is of type 'number', the max
+							possible value of that number,
+						type: (string) 'number' or 'checkbox',
+						value: (type) the default / current value
+					 }
+* }
+* Here are some incomplete examples of the functionality we should handle.
+*
+* /api/files
+* Requests: GET, POST
+* 	GET: Return a listing of files
+* 		RESPONSE JSON: {
+* 			files: [
+* 				{
+* 					name: "Some File",
+* 					size: "123kb",
+* 					file_URI: "/api/files/{file:uid}/download"
+* 			  		thumb_URI: "/api/files/{file:uid}/thumb"
+* 				}
+* 			]
+* 		}
+* 	POST:
+* 		Figure out how to handle single file uploads. https://coligo.io/building-ajax-file-uploader-with-node/
+* 		looks very useful
+*
+*
+* /api/files/{file:uid}
+* Requests: GET
+* 	GET: Return a particular file's metadata
+* 		RESPONSE JSON: {
+* 			file: {
+* 				name: "Some File",
+* 				size: "123kb",
+* 				file_URI: "/api/files/{file:uid}/download',
+* 				thumb_URI: "/api/files/{file:uid}/thumb"
+* 			}
+* 		}
+*/
+const ENDPOINT_URI = '/files';
+
+/**
+ * Register all routes and their supported HTTP request methods
+ * @param  {express.Router()} router Initialized express router to attach routes to
  */
+module.exports.RegisterRoutes = (router) => {
+
+	/**
+	 * /api/files endpoint
+	 * @methods: GET, POST
+	 */
+	router.get(ENDPOINT_URI, (req, res) => {
+		const success = ENDPOINT_URI + ': API GET Request received';
+		console.log(success);
+		res.json({ message: success });
+	});
+
+	router.post(ENDPOINT_URI, (req, res) => {
+		const success = ENDPOINT_URI + ': API POST Request received';
+		console.log(success);
+		res.json({ message: success});
+	});
+
+	/**
+	 * /api/files/:file_uid
+	 * @methods: GET, PATCH, DELETE
+	 */
+}
